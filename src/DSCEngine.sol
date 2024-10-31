7db7ea5b2e02b3b891a8793e57df937df6254307
Bata Zoaka <102160639+Ella-1@users.noreply.github.com>
2024-10-31 13:29:28 +0100

    deposite colleteral function

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-
+import {DecentralizedStableCoin} from "./DecentrilisedStableCoin.sol";
+import {ReentrancyGuard} from "lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 /*
  * @title DSCEngine
  * @author Zoaka Bata Bukar
@@ -22,32 +24,83 @@ pragma solidity 0.8.20;
  * @notice This contract is based on the MakerDAO DSS system
  */
 
-contract DSCEngine {
-    function depositeCollateralAndMIntDSC() external {
-        
+contract DSCEngine is ReentrancyGuard {
+    // Errors
+    error DSCEngine__MoreThanZero();
+    error DSCEngine__TokenPriceFeedsAndPriceFeedsNotTheSameLength();
+    error DSCEngine__NotAllowedToken();
+    error DSCEngine__TransferFailed();
+    // State Variables
+
+    mapping(address => address priceFeeds) private s_pricefeeds; //token to price feeds
+
+    // maps each user to the token they are using and amount
+    mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposits;
+
+    // Events
+    event ColleteralDeposited(address indexed user, address indexed token, uint256 amount);
+    DecentralizedStableCoin private immutable i_dsc;
+    // Modifiers
+
+    modifier MoreThanZero(uint256 amount) {
+        if (amount == 0) {
+            revert DSCEngine__MoreThanZero();
+        }
+        _;
     }
 
-    function depositeColleterall()  external {
-        
+    modifier isTokenAllowed(address token) {
+        if (s_pricefeeds[token] == address(0)) {
+            revert DSCEngine__NotAllowedToken();
+        }
+        _;
     }
 
-    function redeemeCollwtweraForDsc() external {
+    // Functions
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedsAddesses, address dscAddress) {
+        if (tokenAddresses.length != priceFeedsAddesses.length) {
+            revert DSCEngine__TokenPriceFeedsAndPriceFeedsNotTheSameLength();
+        }
 
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_pricefeeds[tokenAddresses[i]] = priceFeedsAddesses[i];
+        }
+
+        i_dsc = DecentralizedStableCoin(dscAddress);
+    }
+    // External Functions
+
+    function depositeCollateralAndMIntDSC() external {}
+
+    /*
+    *@notice follows CEI(chat effects Interaction)
+    *@param tokenColleteralAddress the address of the token to deposit as collateral
+    *@param  amountColleteral the amount of the colleteral to deposit
+    * 
+    */
+    function depositeColleterall(address tokenColleteralAddress, uint256 amountColleteral)
+        external
+        MoreThanZero(amountColleteral)
+        isTokenAllowed(tokenColleteralAddress)
+        nonReentrant
+    {
+        s_collateralDeposits[msg.sender][tokenColleteralAddress] += amountColleteral;
+        emit ColleteralDeposited(msg.sender,tokenColleteralAddress, amountColleteral);
+        bool success = IERC20(tokenColleteralAddress).transferFrom(msg.sender, address(this), amountColleteral);
+        if (!success) {
+            revert DSCEngine__TransferFailed();
+        }
     }
 
-    function redeemeColleteral()  external {
-        
-    }
+    function redeemeCollwtweraForDsc() external {}
 
-    function burnDsc() external { 
+    function redeemeColleteral() external {}
 
-    }
+    function mintDsc(uint256 amountDscToMint) external MoreThanZero(amountDscToMint) {}
 
-    function liquidate() external {
+    function burnDsc() external {}
 
-    }
+    function liquidate() external {}
 
-    function getHealthFactor() external view {
-
-    }
+    function getHealthFactor() external view {}
 }
--- src/DecentrilisedStableCoin.sol
+++ src/DecentrilisedStableCoin.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import { ERC20Burnable, ERC20 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /*
  * @title DecentralizedStableCoin
@@ -19,8 +19,8 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable {
     error DecentralizedStableCoin__AmountMustBeMoreThanZero();
     error DecentralizedStableCoin__BurnAmountExceedsBalance();
     error DecentralizedStableCoin__NotZeroAddress();
-    
-    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) { }
+
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {}
 
     function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
